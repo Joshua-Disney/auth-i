78048580a2d71a394fc76dd8f84518e3a34696f0
joshua-disney <joshua.a.disney@gmail.com>
2019-02-26 18:56:31 -0700

    install express-session and connect-session-knex, implement sessions and cookies, build logout endpoint

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
+const db = require("../data/dbConfig.js");
 const Users = require("./usersModel.js");
 
 const router = express.Router();
+
+const sessionConfig = {
+  name: "vfd",
+  secret: "volunteer fire department",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30
+  })
+};
+
+router.use(session(sessionConfig));
 
 router.post("/register", (req, res) => {
   let user = req.body;
@@ -26,6 +51,7 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Logged in.` });
       } else {
         res.status(401).json({ message: "YOU SHALL NOT PASS!" });
@@ -37,25 +63,33 @@ router.post("/login", (req, res) => {
 });
 
 function restricted(req, res, next) {
-  const { username, password } = req.body;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "Invalid Credentials" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: "Ran into an unexpected error" });
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: "No credentials provided" });
+    res.status(401).json({ message: "YOU SHALL NOT PASS!" });
   }
 }
+
+// function restricted(req, res, next) {
+//   const { username, password } = req.body;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "Invalid Credentials" });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json({ message: "Ran into an unexpected error" });
+//       });
+//   } else {
+//     res.status(400).json({ message: "No credentials provided" });
+//   }
+// }
 
 router.get("/users", restricted, async (req, res) => {
   try {
@@ -63,6 +97,20 @@ router.get("/users", restricted, async (req, res) => {
     res.json(users);
   } catch (error) {
     res.send(error);
+  }
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.send("I'm sorry, Dave. I'm afraid I can't do that.");
+      } else {
+        res.send("Bye!  Hhave a beautiful time!");
+      }
+    });
+  } else {
+    res.end();
   }
 });
 
